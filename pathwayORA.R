7f695b6040e79554fd676234935cec09d2c8c450
DannyArends <Danny.Arends@gmail.com>
2024-06-18 11:23:03 +0100

    Automated pathway ORA using the clusterProfiler package

--- pathwayORA.R
+++ pathwayORA.R
@@ -3,6 +3,8 @@ setwd("/home/rqdt9/Dropbox (UTHSC GGI)/MyFolder/UM-HET3")
 regions <- read.table("regions_4way_merged_May24.txt", sep="\t", header=FALSE, row.names=1)
 colnames(regions) <- c("Chr", "Proximal", "Distal", "Old")
 
+### Pathway ORA all genes
+
 protein_coding <- vector("list", nrow(regions))
 prioritized <- vector("list", nrow(regions))
 
@@ -24,6 +26,49 @@ for(x in 1:nrow(regions)){
 cat(unique(bg), file = "ORAbackground.txt",sep="\n", collapse = "\n")
 cat(unique(prio), file = "ORAprio.txt",sep="\n", collapse = "\n")
 cat(unique(prioH), file = "ORAprioH.txt",sep="\n", collapse = "\n")
+length(unique(bg))
+length(unique(prio))
+length(unique(prioH))
+
+### Pathway ORA no GPRCs
+
+protein_coding <- vector("list", nrow(regions))
+prioritized <- vector("list", nrow(regions))
+GPCRs <- read.table("May2024/GPCRs.txt", sep = "\t", header=TRUE)
+
+bg <- c()
+prio <- c()
+prioH <- c()
+for(x in 1:nrow(regions)){
+  protein_coding[[x]] <- read.csv(paste0("May2024/genes/protein_coding_genes_", rownames(regions)[x], ".txt"), sep = "\t")
+  iix <- which(protein_coding[[x]][, "ensembl_gene_id"] %in% GPCRs[,"Ensembl.Gene.ID..Mouse."])
+  protein_coding[[x]] <- protein_coding[[x]][-iix,]
+  olf <- grep("^Olf", protein_coding[[x]][,"external_gene_name"])
+  if(length(olf)> 0) protein_coding[[x]] <- protein_coding[[x]][-olf,]
+
+  prioritized[[x]] <- read.csv(paste0("May2024/summary/", rownames(regions)[x], ".summary.txt"), sep = "\t")
+  iix <- which(prioritized[[x]][, "ensembl_gene_id"] %in% GPCRs[,"Ensembl.Gene.ID..Mouse."])
+  prioritized[[x]] <- prioritized[[x]][-iix,]
+  olf <- grep("^Olf", prioritized[[x]][,"name"])
+  if(length(olf)> 0) protein_coding[[x]] <- prioritized[[x]][-olf,]
+
+  iiHigh <- which(prioritized[[x]][,3] == "HIGH")
+  iiMod <- which(prioritized[[x]][,3] != "NONE")
+
+  bg <- c(bg, protein_coding[[x]][,1])
+  prio <- c(prio, prioritized[[x]][iiMod,1])
+  prioH <- c(prioH, prioritized[[x]][iiHigh,1])
+}
+
+cat(unique(bg), file = "ORAbackground_noGPCRs.txt",sep="\n", collapse = "\n")
+cat(unique(prio), file = "ORAprio_noGPCRs.txt",sep="\n", collapse = "\n")
+cat(unique(prioH), file = "ORAprioH_noGPCRs.txt",sep="\n", collapse = "\n")
+length(unique(bg))
+length(unique(prio))
+length(unique(prioH))
+
+
+## OLD ORA (previous version of Vita regions)
 
 regions <- read.table("regions_4way_merged_March23.txt", sep="\t", header=FALSE, row.names=1)
 colnames(regions) <- c("Chr", "Proximal", "Distal", "Old")
